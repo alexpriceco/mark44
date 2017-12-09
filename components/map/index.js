eed0a257a2d34b8521f4cdcca61c1a892023bebb
Alex Price <alex@alexprice.co>
2017-12-09 13:25:57 -0600

    Adds working multilayer w/ Maine example

--- components/map/index.js
+++ components/map/index.js
@@ -17,37 +17,63 @@ export class Map extends Component {
     if (process.browser) {
       const ReactMapboxGlLibrary = require('react-mapbox-gl')
       this.ReactMapboxGl = ReactMapboxGlLibrary.default
+      this.Layer = ReactMapboxGlLibrary.Layer
+      this.Feature = ReactMapboxGlLibrary.Feature
+
+      this.Map = this.ReactMapboxGl({ accessToken: MAPBOX_ACCESS_TOKEN })
     }
   }
 
-  componentDidMount () {
-  }
-
   render () {
-    if (this.ReactMapboxGl) {
-      const Map = this.ReactMapboxGl({
-        // if this is breaking, make sure to check step X in the readme
-        accessToken: MAPBOX_ACCESS_TOKEN
-      })
-
+    const { Map, Layer, Feature } = this
+    if (Map && Layer && Feature) {
       return (
         <div>
           <Map
+            center={this.state.center}
             style='mapbox://styles/alexprice/cjazmeo05puoy2sqvx8o999lj'
             containerStyle={{
               height: '100vh',
               width: 'calc(100vw - 32em)'
             }}
-            onMove={(e) => console.debug('onMove', e)}
-            onClick={(e) => console.debug('onClick', e)}
-            center={this.state.center}
-          />
+          >
+          <Layer
+            type='fill'
+            paint={{
+              'fill-color': '#6F788A',
+              'fill-opacity': 0.7
+            }}
+          >
+            <Feature coordinates={[[
+              [-67.13734351262877, 45.137451890638886],
+              [-66.96466, 44.8097],
+              [-68.03252, 44.3252],
+              [-69.06, 43.98],
+              [-70.11617, 43.68405],
+              [-70.64573401557249, 43.090083319667144],
+              [-70.75102474636725, 43.08003225358635],
+              [-70.79761105007827, 43.21973948828747],
+              [-70.98176001655037, 43.36789581966826],
+              [-70.94416541205806, 43.46633942318431],
+              [-71.08482, 45.3052400000002],
+              [-70.6600225491012, 45.46022288673396],
+              [-70.30495378282376, 45.914794623389355],
+              [-70.00014034695016, 46.69317088478567],
+              [-69.23708614772835, 47.44777598732787],
+              [-68.90478084987546, 47.184794623394396],
+              [-68.23430497910454, 47.35462921812177],
+              [-67.79035274928509, 47.066248887716995],
+              [-67.79141211614706, 45.702585354182816],
+              [-67.13734351262877, 45.137451890638886]
+            ]]} />
+          </Layer>
+          </Map>
 
           <Style sheet={sheet} />
         </div>
       )
     } else return (
-      <Loader />
+      <Loader key={new Date().toISOString()} />
     )
   }
 }
