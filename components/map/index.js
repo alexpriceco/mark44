78e921b038d1f8b509e37c1df19f6cffc58aeb8e
Alex Price <alex@alexprice.co>
2017-12-09 11:17:27 -0600

    Adds MapboxGL

--- components/map/index.js
+++ components/map/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
 
 import Loader from '../general/loader'
 import Style from '../general/style'
@@ -15,10 +16,26 @@ export class Map extends Component {
   componentDidMount () {
   }
 
+  const Map = ReactMapboxGl({
+    accessToken: MAPBOX_ACCESS_TOKEN
+  })
+
   render () {
     return (
       <div>
-        MAP GOES HERE
+        <Map
+        style='mapbox://styles/mapbox/streets-v9'
+        containerStyle={{
+          height: '100vh',
+          width: '80vw'
+        }}>
+          <Layer
+            type='symbol'
+            id='marker'
+            layout={{ 'icon-image': 'marker-15' }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
+        </Map>
         <Style sheet={sheet} />
       </div>
     )
